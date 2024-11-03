cf162d211668bd44d306444bc7dd8d49081a4ed1
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2024-11-03 12:46:17 +0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -1,1 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
 
+df = pd.read_csv("https://raw.githubusercontent.com/R8522/sem7Data/refs/heads/main/Patient%20Survival%20Prediction.csv")
+df.head()
+
+st.write(df)
