40fb745a99a6bac69f975616e617d17e945c3828
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2024-11-03 13:26:43 +0800

    Create Decision.py

---
+++ pages/Decision.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("https://raw.githubusercontent.com/R8522/sem7Data/refs/heads/main/Patient%20Survival%20Prediction.csv")
+df.head()
+
+st.write(df)
+
